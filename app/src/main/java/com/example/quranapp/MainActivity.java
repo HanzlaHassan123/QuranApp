5ff8f248d3ec7ebf904828cb81c444ad50fbc52a
Hanzla Tarar <tararhanzla783@gmail.com>
2023-06-05 10:45:21 -0700

    when user selects the surah it will take it to the next activity and also i created the basic design of second activity in which a range for ayat in surah is specified

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quranapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -37,13 +40,19 @@ public class MainActivity extends AppCompatActivity {
            combinedList.add(combinedItem);
        }
 
-
-
-
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, combinedList);
         surahNames.setAdapter(adapter);
 
+        surahNames.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem=(String) parent.getItemAtPosition(position);
+                Intent intent= new Intent(MainActivity.this,Surahs.class);
+                intent.putExtra("selectedSurah",selectedItem);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/quranapp/Surahs.java
@@ -0,0 +1,15 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Surahs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surahs);
+
+    }
+}
